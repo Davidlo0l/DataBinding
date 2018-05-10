cd27123e07cf29171a8e1fd016993a2541eb4731
Davidlo0l <38057704+Davidlo0l@users.noreply.github.com>
2018-05-09 19:09:22 -0500

    propiedades

--- DataBinding/Persona.cs
+++ DataBinding/Persona.cs
@@ -41,16 +41,47 @@ using System.Text;
                 OnPropertyChanged("Pais");
             }
         }
+    
+
+        public decimal Saldo
+        {
+            get { return saldo; }
+            set {
+                if (Saldo == value)
+                {
+                    return;
+                }
+                saldo = value;
+                OnPropertyChanged("saldo");
+            }
+        }
+
+        private DateTime fechaNacimiento;
+
+        public DateTime FechaNacimiento
+        {
+            get { return fechaNacimiento; }
+            set {
+                if (fechaNacimiento == value)
+                {
+                    return;
+                }
+                fechaNacimiento = value;
+                OnPropertyChanged("fecha de Nacimiento");
+            }
+        }
+
         #endregion
 
         #region Atributos
         private string nombre;
         private string pais;
+        private decimal saldo;
         #endregion
         #region Metodos
         public override string ToString()
         {
-            return $"{Nombre} | {Pais}";
+            return $"{Nombre} | {Pais} |{ Saldo} {fechaNacimiento}|";
         }
         #endregion
 
