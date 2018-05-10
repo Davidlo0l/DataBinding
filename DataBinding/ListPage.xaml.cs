ad5235d775be0335153b50b4ee655c28169a857c
Davidlo0l <38057704+Davidlo0l@users.noreply.github.com>
2018-05-09 21:20:05 -0500

    nuevos

---
+++ DataBinding/ListPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DataBinding
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ListPage : ContentPage
+	{
+        public ListPage()
+        {
+            InitializeComponent();
+            Paginas = new List<Page>()
+            {
+                new BindingList(),
+                new MainPage()
+
+            };
+            this.BindingContext = this;
+            list.ItemSelected += List_ItemSelected;
+           
+        }
+
+        private void List_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                this.Navigation.PushAsync((Page)e.SelectedItem);
+            }
+            list.SelectedItem = null;
+        }
+
+        public List<Page> Paginas { get; set; }
+
+      
+    }
+}
