841127c621792fc770199cd9488aa2edaf73b6aa
Davidlo0l <38057704+Davidlo0l@users.noreply.github.com>
2018-05-09 21:22:27 -0500

    using

--- DataBinding/ListPage.xaml.cs
+++ DataBinding/ListPage.xaml.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿
 
 namespace DataBinding
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ListPage : ContentPage
 	{
         public ListPage()
